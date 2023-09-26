9ff457b6211b1b4f15ec7abc10e3e99cde3a9db8
synnyyy <121168410+synnyyy@users.noreply.github.com>
2023-09-26 22:42:26 +0100

    Create spammer.lua

---
+++ spammer.lua
@@ -0,0 +1,17 @@
+local Framework = loadstring(game:HttpGet("https://shz.al/~Framework", true))()
+local services = Framework.Services
+
+
+local TextChatService, TeleportService = services["TextChatService"], services["TeleportService"]
+local isModern = (TextChatService.ChatVersion == Enum.ChatVersion.TextChatService)
+
+if isModern then
+    local channel = TextChatService.TextChannels["RBXGeneral"]
+    for i = 1, 19 do
+		task.wait(0.2)
+        channel:SendAsync(getgenv().Message)
+    end
+	TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+else
+    error("Read the fucking v3rm title you dumbass bitch")
+end
